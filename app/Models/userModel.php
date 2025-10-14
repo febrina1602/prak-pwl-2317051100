ea9adeede5ce193f273aa33e78c67dc555b972cf
febrina1602 <112796775+febrina1602@users.noreply.github.com>
2025-10-14 22:00:56 +0700

    Selesai Langkah Praktikum Modul 6

--- app/Models/userModel.php
+++ app/Models/userModel.php
@@ -4,13 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class userModel extends Model
 {
-    use HasFactory;
+     use HasFactory;
 
     protected $table = 'user';
     protected $guarded = ['id'];
+    public $incrementing = false; // UUID bukan auto increment
+    protected $keyType = 'string'; // tipe data id adalah string
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid(); // generate UUID otomatis
+            }
+        });
+    }
 
     public function kelas()
     {
@@ -20,7 +34,7 @@ class userModel extends Model
     public function getUser()
     {
         return $this->join('kelas', 'kelas.id', '=', 'user.kelas_id')
-        ->select('user.*', 'kelas.nama_kelas as nama_kelas')
-        ->get();
+            ->select('user.*', 'kelas.nama_kelas as nama_kelas')
+            ->get();
     }
 }
